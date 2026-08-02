5f193a621fa71df9b4727a87fa34915814a2d547
Isabella <24170045@uttcampus.edu.mx>
2026-08-02 12:28:26 -0600

    Agregué los métodos `save` en el cual me falta agregar los dema atributos de alumno y `getAll` en la clase `Alumno` para manejar persistencia y recuperación desde la base de datos

--- src/modelos/Alumno.java
+++ src/modelos/Alumno.java
@@ -1,5 +1,11 @@
 package modelos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Alumno {
     private int id;
     private String matricula;
@@ -17,6 +23,41 @@ public class Alumno {
         this.correo = correo;
     }
 
+    //metodos
+    public void save()throws Exception {
+        try (Connection connection = Conexión.getConexion();
+             PreparedStatement stmt = connection.prepareStatement(("INSERT INTO Alumnos(nombre) values (?)  "), java.sql.Statement.RETURN_GENERATED_KEYS);
+             // el java sql es para que regresa la llave que se genero
+        ) {
+            stmt.setString(1, this.nombre); // this. nombre se saca del atributo del objeto alumno
+            stmt.executeUpdate(); //avisa que traiga las fk
+            ResultSet rs = stmt.getGeneratedKeys(); //dame las llaves foraneas que trajiste y genera el result set
+            {
+                if (rs.next()) {
+                    this.id = rs.getInt(1);
+                }
+            }
+        }
+    }
+    public static List<Alumno> getAll() throws Exception {
+        try(
+                Connection connection = Conexión.getConexion();
+                PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Alumnos");
+        )
+        {
+            ResultSet rs = stmt.executeQuery();
+            List<Alumno> alumnos = new ArrayList<>();
+            while (rs.next()) {
+                Alumno a = new Alumno(rs.getInt("id"),(rs.getString("matricula")),
+                        (rs.getString("nombre")),(rs.getInt("edad")),
+                        (rs.getString("sexo")),(rs.getString("correo")));
+                alumnos.add(a);
+            }
+            return alumnos;
+        }
+    }
+
+    //getters y setters
     public int getId() {
         return id;
     }
