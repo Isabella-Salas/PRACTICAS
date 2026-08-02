c1541492639eba38364caeaf3d50649549b4ec82
Isabella <24170045@uttcampus.edu.mx>
2026-08-02 01:11:07 -0600

    Añadí configuración de biblioteca JDBC y clase inicial para conexión a la base de datos

---
+++ src/Conexión.java
@@ -0,0 +1,10 @@
+public class Conexión {
+
+    static String server = "localhost";
+    static int port = 1433;
+    static String database = "Practica_Alumno";
+    static String user = "sa";
+    static String password = "isa2006";
+
+
+}
