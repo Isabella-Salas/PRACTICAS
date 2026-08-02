c03f4f60d9c5f40c9944d367b33970afff9225dd
Isabella <24170045@uttcampus.edu.mx>
2026-08-02 01:30:38 -0600

    Añadí métodos getter y setter para los atributos de la clase `Alumno`

--- src/modelos/Alumno.java
+++ src/modelos/Alumno.java
@@ -17,4 +17,51 @@ public class Alumno {
         this.correo = correo;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
