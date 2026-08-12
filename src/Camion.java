dfdd6aec7f7a1832ee94d09f251824c3e77d2b2a
Isabella <24170045@uttcampus.edu.mx>
2026-08-12 09:27:10 -0600

    Agregué constructores en `Vehiculo` y sus clases derivadas `Moto`, `Automovil` y `Camion` para inicializar atributos principales.

--- src/Camion.java
+++ src/Camion.java
@@ -1,2 +1,6 @@
 public class Camion extends Vehiculo{
+
+    public Camion(String placa, int horas_Estacionamiento, Tarifa tarifa) {
+        super(placa, horas_Estacionamiento, tarifa);
+    }
 }
--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -4,6 +4,12 @@ public abstract class Vehiculo {
     private int horas_Estacionamiento;
     private Tarifa tarifa;
 
+    public Vehiculo(String placa, int horas_Estacionamiento, Tarifa tarifa) {
+        this.placa = placa;
+        this.horas_Estacionamiento = horas_Estacionamiento;
+        this.tarifa = tarifa;
+    }
+
 
 
 }
