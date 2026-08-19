723fce0992fe5c23145ad741da32914bf7880a96
Isabella <24170045@uttcampus.edu.mx>
2026-08-19 08:30:10 -0600

    Actualicé el nombre de la base de datos en la clase `Conexión` de `Practica_Estacionamiento` a `VEHICULOS`.

--- src/modelos/Conexión.java
+++ src/modelos/Conexión.java
@@ -8,7 +8,7 @@ public class Conexión {
 
     static String server = "localhost";
     static int port = 1433;
-    static String database = "Practica_Estacionamiento";
+    static String database = "VEHICULOS";
     static String user = "sa";
     static String password = "isa2006";
 
